aba55797d18b2121e7ae48f6c3756fb94f0626d0
minturam21 <ramchiarymintu85@gmail.com>
2025-12-10 15:24:37 +0530

    preprocess file

---
+++ utils/preprocess.py
@@ -0,0 +1,12 @@
+from typing import List
+import numpy as np
+
+
+def preprocess_features(features: List[float]):
+    """
+    Adapt this to match the preprocessing you used during training.
+    Right now it's just converting to a 2D numpy array.
+    """
+    X = np.array(features, dtype=float).reshape(1, -1)
+    # TODO: apply scaling/encoding if you used them in training
+    return X
